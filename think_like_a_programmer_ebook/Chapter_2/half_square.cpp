c2b15e52fe1d4d51b95cb5ca382598a52ef911a2
cerbin1 <gekarez@gmail.com>
2024-01-17 09:15:35 +0100

    formattter applied

--- think_like_a_programmer_ebook/Chapter_2/half_square.cpp
+++ think_like_a_programmer_ebook/Chapter_2/half_square.cpp
@@ -5,25 +5,25 @@ using std::cout;
 using namespace std;
 
 string halfASquare() {
-    string result = "";
+  string result = "";
 
-  for(int row = 1; row <= 5; row++) {
-      for(int numberOfHashes = 6 - row; numberOfHashes >= 1; numberOfHashes--) {
-          result += "#";
-      }
-      result += "\n";
+  for (int row = 1; row <= 5; row++) {
+    for (int numberOfHashes = 6 - row; numberOfHashes >= 1; numberOfHashes--) {
+      result += "#";
     }
+    result += "\n";
+  }
 
-    return result;
+  return result;
 }
 
 int main() {
-    string result = halfASquare();
-    if (result == "#####\n####\n###\n##\n#\n") {
-        cout << result;
-    } else {
-        throw runtime_error ("error");
-    }
+  string result = halfASquare();
+  if (result == "#####\n####\n###\n##\n#\n") {
+    cout << result;
+  } else {
+    throw runtime_error("error");
+  }
 
-    return 0;
+  return 0;
 }
