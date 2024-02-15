0e9d633c0da08ec6ae66a8582b81f3dbbc789eeb
cerbin1 <gekarez@gmail.com>
2024-02-15 11:37:23 +0100

    Fix task 2.1 - did not copy spaces to answer output, fix it and finish answer

--- Chapter_2 Pure Puzzles/2.1.cpp
+++ Chapter_2 Pure Puzzles/2.1.cpp
@@ -7,15 +7,18 @@ Using the same rule as the shapes programs from earlier in the chapter (only
 two output statements—one that outputs the hash mark and one that outputs
 an end-of-line), write a program that produces the following shape:
 ########
-######
-####
-##
+ ######
+  ####
+   ##
 *******************************************/
 
 string halfASquare() {
   string result = "";
 
   for (int row = 1; row < 5; row++) {
+    for (int numberOfSpaces = 1; numberOfSpaces < row; numberOfSpaces++) {
+      result += ' ';
+    }
     for (int numberOfHashes = 10 - row * 2; numberOfHashes >= 1;
          numberOfHashes--) {
       result += "#";
@@ -28,7 +31,7 @@ string halfASquare() {
 
 int main() {
   string result = halfASquare();
-  if (result == "########\n######\n####\n##\n") {
+  if (result == "########\n ######\n  ####\n   ##\n") {
     cout << result;
   } else {
     throw runtime_error("error");
